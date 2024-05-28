6233f10aa48a4043770969530fdf016941df898b
Pedrobk222 <pedro.bortolucci1.ph@gmail.com>
2024-05-28 14:15:15 -0300

    Adicionando a rotaget pontos. (hcf(id), latitude, longitude, cidade)

--- src/controllers/cidades-controller.js
+++ src/controllers/cidades-controller.js
@@ -1,15 +1,14 @@
 import models from '../models';
 
-const {
-    Cidade,
-} = models;
+const { Cidade, LocalColeta, Tombo } = models;
 
-export const listaTodosCidades = where => Cidade.findAndCountAll({
-    attributes: {
-        exclude: ['updated_at', 'created_at'],
-    },
-    where,
-});
+export const listaTodosCidades = where =>
+    Cidade.findAndCountAll({
+        attributes: {
+            exclude: ['updated_at', 'created_at'],
+        },
+        where,
+    });
 
 export const listagem = (request, response, next) => {
     let where = {};
@@ -25,4 +24,30 @@ export const listagem = (request, response, next) => {
             response.status(200).json(cidades.rows);
         })
         .catch(next);
+};
+
+export const listaTodosOsTombosComLocalizacao = async (req, res, next) => {
+    try {
+        const tombos = await Tombo.findAll({
+            attributes: ['hcf', 'latitude', 'longitude'],
+            include: {
+                model: LocalColeta,
+                include: {
+                    model: Cidade,
+                    attributes: ['nome'],
+                },
+            },
+        });
+
+        const result = tombos.map(tombo => ({
+            hcf: tombo.hcf,
+            latitude: tombo.latitude,
+            longitude: tombo.longitude,
+            cidade: tombo.locais_coletum && tombo.locais_coletum.cidade ? tombo.locais_coletum.cidade.nome : null,
+        }));
+
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
 };
--- src/routes/cidades.js
+++ src/routes/cidades.js
@@ -1,9 +1,7 @@
 const controller = require('../controllers/cidades-controller');
 
 export default app => {
+    app.route('/cidades').get([controller.listagem]);
 
-    app.route('/cidades')
-        .get([
-            controller.listagem,
-        ]);
+    app.route('/pontos').get([controller.listaTodosOsTombosComLocalizacao]);
 };
