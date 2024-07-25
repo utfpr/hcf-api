dc1ae2b755bfa47b8f6052e4ac8e834392194fa8
RyanRamos01 <ryan.lramos@hotmail.com>
2024-07-25 13:45:07 -0300

    fix: comentários do pr resolvidos (um pendente)

--- src/middlewares/ordenacao-middleware.js
+++ src/middlewares/ordenacao-middleware.js
@@ -16,52 +16,46 @@ const criaListagemMiddleware = (allowedColumns, defaultSort = 'nome', defaultDir
 
     const [column, direction] = order.split(':');
 
-    if (!(column === defaultSort)) {
-        if (!allowedColumns.includes(column)) {
-            return response.status(400).json({ error: 'Coluna inválida' });
-        }
+    if (column !== defaultSort && !allowedColumns.includes(column)) {
+        return response.status(400).json({ error: 'Coluna inválida' });
     }
 
-    if (!(direction === defaultDirection)) {
-        if (!['asc', 'desc'].includes(direction)) {
-            return response.status(400).json({ error: 'Direção inválida' });
-        }
+    if (!(direction === defaultDirection) && !['asc', 'desc'].includes(direction)) {
+        return response.status(400).json({ error: 'Direção inválida' });
     }
 
     let orderClause = [[defaultSort, defaultDirection.toUpperCase()]];
 
-    if (allowedColumns.includes(column)) {
-        if (column === 'autor') {
-            orderClause = [['nome', direction.toUpperCase()]];
+    if (column === 'autor') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/autores')) orderClause = [[{ model: Autor, as: 'autor' }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'familia') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/autores')) orderClause = [[{ model: Autor, as: 'autor' }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'familia') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/familias')) orderClause = [[{ model: Familia }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'subfamilia') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/familias')) orderClause = [[{ model: Familia }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'subfamilia') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/subfamilias')) orderClause = [[{ model: Subfamilia }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'genero') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/subfamilias')) orderClause = [[{ model: Subfamilia }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'genero') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/generos')) orderClause = [[{ model: Genero }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'especie') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/generos')) orderClause = [[{ model: Genero }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'especie') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/especies')) orderClause = [[{ model: Especie, as: 'especie' }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'subespecie') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/especies')) orderClause = [[{ model: Especie, as: 'especie' }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'subespecie') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/subespecies')) orderClause = [[{ model: Subespecie }, 'nome', direction.toUpperCase()]];
-        } else if (column === 'variedade') {
-            orderClause = [['nome', direction.toUpperCase()]];
+        if (!request.path.includes('/subespecies')) orderClause = [[{ model: Subespecie }, 'nome', direction.toUpperCase()]];
+    } else if (column === 'variedade') {
+        orderClause = [['nome', direction.toUpperCase()]];
 
-            if (!request.path.includes('/variedades')) orderClause = [[{ model: Variedade }, 'nome', direction.toUpperCase()]];
-        } else {
-            orderClause = [[column, direction.toUpperCase()]];
-        }
+        if (!request.path.includes('/variedades')) orderClause = [[{ model: Variedade }, 'nome', direction.toUpperCase()]];
+    } else {
+        orderClause = [[column, direction.toUpperCase()]];
     }
 
     request.ordenacao = {
