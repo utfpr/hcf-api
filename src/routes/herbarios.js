1b53be5e2ee3649efb2c5a4b206b9da16c808187
Pedrobk222 <pedro.bortolucci1.ph@gmail.com>
2024-08-19 21:13:41 -0300

    ajustando lint

--- src/routes/herbarios.js
+++ src/routes/herbarios.js
@@ -9,13 +9,37 @@ import listagemHerbarioEsquema from '../validators/herbario-listagem';
 const controller = require('../controllers/herbarios-controller');
 
 export default app => {
-    app
-        .route('/herbarios')
-        .post([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(cadastrarHerbarioEsquema), controller.cadastro])
-        .get([listagensMiddleware, validacoesMiddleware(listagemHerbarioEsquema), controller.listagem]);
-    app
-        .route('/herbarios/:herbario_id')
-        .put([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(atualizarHerbarioEsquema), controller.editar])
-        .delete([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(desativarHerbarioEsquema), controller.desativar])
-        .get([validacoesMiddleware(desativarHerbarioEsquema), controller.buscarHerbario]);
+
+    app.route('/herbarios')
+        .post([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(cadastrarHerbarioEsquema),
+            controller.cadastro,
+        ])
+        .get([
+            listagensMiddleware,
+            validacoesMiddleware(listagemHerbarioEsquema),
+            controller.listagem,
+        ]);
+    app.route('/herbarios/:herbario_id')
+        .put([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(atualizarHerbarioEsquema),
+            controller.editar,
+        ])
+        .delete([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(desativarHerbarioEsquema),
+            controller.desativar,
+        ])
+        .get([
+            validacoesMiddleware(desativarHerbarioEsquema),
+            controller.buscarHerbario,
+        ]);
 };
