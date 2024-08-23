3081b17a0a8c416ed42d3db4a6b005941a7f838e
Pedrobk222 <pedro.bortolucci1.ph@gmail.com>
2024-08-23 18:08:41 -0300

    arrumando lint

--- src/routes/tombos.js
+++ src/routes/tombos.js
@@ -1,24 +1,10 @@
 import { ListaTodosOsTombosComLocalizacao } from '../controllers/cidades-controller';
 import fichaTomboController from '../controllers/fichas-tombos-controller';
 import {
-    getDadosCadTombo,
-    getNumeroTombo,
-    cadastro,
-    listagem,
-    desativar,
-    obterTombo,
-    cadastrarTipo,
-    buscarTipos,
-    cadastrarColetores,
-    buscarColetores,
-    buscarProximoNumeroColetor,
-    alteracao,
-    getNumeroColetor,
-    getUltimoNumeroTombo,
-    getCodigoBarraTombo,
-    editarCodigoBarra,
-    deletarCodigoBarra,
-    getUltimoNumeroCodigoBarras,
+    getDadosCadTombo, getNumeroTombo, cadastro, listagem,
+    desativar, obterTombo, cadastrarTipo, buscarTipos, cadastrarColetores, buscarColetores,
+    buscarProximoNumeroColetor, alteracao, getNumeroColetor, getUltimoNumeroTombo, getCodigoBarraTombo,
+    editarCodigoBarra, deletarCodigoBarra, getUltimoNumeroCodigoBarras,
 } from '../controllers/tombos-controller';
 import exportarTombosController from '../controllers/tombos-exportacoes-controller';
 import criaJsonMiddleware from '../middlewares/json-middleware';
@@ -31,50 +17,132 @@ import cadastrarTomboEsquema from '../validators/tombo-cadastro';
 import listagemTombo from '../validators/tombo-listagem';
 
 export default app => {
-    app.route('/tombos/dados').get([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR, TIPOS_USUARIOS.IDENTIFICADOR]), getDadosCadTombo]);
 
-    app.route('/tombos/numeroColetor/:idColetor').get([getNumeroColetor]);
+    app.route('/tombos/dados')
+        .get([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR,
+                TIPOS_USUARIOS.OPERADOR,
+                TIPOS_USUARIOS.IDENTIFICADOR,
+            ]),
+            getDadosCadTombo,
+        ]);
 
-    app.route('/tombos/codBarras/:idTombo').get([getCodigoBarraTombo])
-        .delete([deletarCodigoBarra]);
+    app.route('/tombos/numeroColetor/:idColetor')
+        .get([
+            getNumeroColetor,
+        ]);
 
-    app.route('/tombos/codBarras').put([editarCodigoBarra]);
+    app.route('/tombos/codBarras/:idTombo')
+        .get([
+            getCodigoBarraTombo,
+        ])
+        .delete([
+            deletarCodigoBarra,
+        ]);
 
-    app.route('/tombos/MaxcodBarras/:emVivo').put([getUltimoNumeroCodigoBarras]);
+    app.route('/tombos/codBarras')
+        .put([
+            editarCodigoBarra,
+        ]);
 
-    app.route('/tombos/exportar').get([listagensMiddleware, criaJsonMiddleware(['campos', 'filtros']), exportarTombosController]);
+    app.route('/tombos/MaxcodBarras/:emVivo')
+        .put([
+            getUltimoNumeroCodigoBarras,
+        ]);
 
-    app.route('/tombos/filtrar_numero/:id').get([getNumeroTombo]);
+    app.route('/tombos/exportar')
+        .get([
+            listagensMiddleware,
+            criaJsonMiddleware([
+                'campos',
+                'filtros',
+            ]),
+            exportarTombosController,
+        ]);
 
-    app.route('/tombos/filtrar_ultimo_numero').get([getUltimoNumeroTombo]);
+    app.route('/tombos/filtrar_numero/:id')
+        .get([
+            getNumeroTombo,
+        ]);
 
-    app
-        .route('/tombos')
-        .post([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(cadastrarTomboEsquema), cadastro])
-        .get([listagensMiddleware, validacoesMiddleware(listagemTombo), listagem]);
+    app.route('/tombos/filtrar_ultimo_numero')
+        .get([
+            getUltimoNumeroTombo,
+        ]);
 
-    app
-        .route('/tombos/:tombo_id')
-        .put([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR, TIPOS_USUARIOS.IDENTIFICADOR]), alteracao])
-        .delete([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), desativar])
-        .get([listagensMiddleware, obterTombo]);
+    app.route('/tombos')
+        .post([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR,
+                TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(cadastrarTomboEsquema),
+            cadastro,
+        ])
+        .get([
+            listagensMiddleware,
+            validacoesMiddleware(listagemTombo),
+            listagem,
+        ]);
 
-    app
-        .route('/tipos')
-        .post([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(cadastrarTipoEsquema), cadastrarTipo])
-        .get([buscarTipos]);
+    app.route('/tombos/:tombo_id')
+        .put([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR,
+                TIPOS_USUARIOS.OPERADOR,
+                TIPOS_USUARIOS.IDENTIFICADOR,
+            ]),
+            alteracao,
+        ])
+        .delete([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR,
+                TIPOS_USUARIOS.OPERADOR,
+            ]),
+            desativar,
+        ])
+        .get([
+            listagensMiddleware,
+            obterTombo,
+        ]);
 
-    app
-        .route('/coletores')
-        .post([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), validacoesMiddleware(coletorCadastro), cadastrarColetores])
-        .get([buscarColetores]);
+    app.route('/tipos')
+        .post([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(cadastrarTipoEsquema),
+            cadastrarTipo,
+        ])
+        .get([
+            buscarTipos,
+        ]);
 
-    app.route('/numero-coletores').get([tokensMiddleware([TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR]), buscarProximoNumeroColetor]);
+    app.route('/coletores')
+        .post([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            validacoesMiddleware(coletorCadastro),
+            cadastrarColetores,
+        ])
+        .get([
+            buscarColetores,
+        ]);
 
-    app.route('/fichas/tombos/:tombo_id/:imprimir_cod').get(fichaTomboController);
+    app.route('/numero-coletores')
+        .get([
+            tokensMiddleware([
+                TIPOS_USUARIOS.CURADOR, TIPOS_USUARIOS.OPERADOR,
+            ]),
+            buscarProximoNumeroColetor,
+        ]);
+
+    app.route('/fichas/tombos/:tombo_id/:imprimir_cod')
+        .get(fichaTomboController);
 
     // app.route('/fichas/tombos/:tombo_id')
     //     .get(fichaTomboController);
-
     app.route('/pontos').get([listagensMiddleware, ListaTodosOsTombosComLocalizacao]);
 };
