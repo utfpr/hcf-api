1e033b5a04f1aeee620161dceaaf36d247f900a8
Edvaldo Szymonek <edvaldoszy@gmail.com>
2025-04-17 19:28:10 -0300

    Cria estrutura para geração de relatórios com React (#71)

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ import morgan from 'morgan';
 
 import { assets, upload } from './config/directory';
 import errors from './middlewares/erros-middleware';
+import { generatePreview, reportPreview } from './reports/controller';
 import routes from './routes';
 
 const app = express();
 app.use(cors());
-app.use(parser.json({ limit: '1mb' }));
+app.use(parser.json());
 app.use(morgan('dev'));
 
 app.use('/fotos', express.static(upload));
 app.use('/assets', express.static(assets));
+
+app.get('/reports/:fileName', reportPreview);
+app.post('/reports/:fileName', generatePreview);
 
 app.use('/api', routes);
 
