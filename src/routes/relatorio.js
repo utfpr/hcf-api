08b46e7834f5890bd4bc9a870a48d145e7486bf8
Matheus Coitinho <mhcl0303@gmail.com>
2025-05-29 20:50:40 -0300

    adiciona middleware de tokens para controle de acesso nas rotas

--- src/routes/relatorio.js
+++ src/routes/relatorio.js
@@ -1,4 +1,5 @@
 import listagensMiddleware from '../middlewares/listagens-middleware';
+import tokensMiddleware, { TIPOS_USUARIOS } from '../middlewares/tokens-middleware';
 
 /// relatorio/inventario-especies
 const controller = require('../controllers/relatorios-controller');
@@ -6,18 +7,33 @@ const controller = require('../controllers/relatorios-controller');
 export default app => {
     app.route('/relatorio/inventario-especies')
         .get([
+            tokensMiddleware([
+            TIPOS_USUARIOS.CURADOR, 
+            TIPOS_USUARIOS.OPERADOR, 
+            TIPOS_USUARIOS.IDENTIFICADOR,
+            ]),
             listagensMiddleware,
             controller.obtemDadosDoRelatorioDeInventarioDeEspecies,
         ]);
 
     app.route('/relatorio/inventario-especies')
         .post([
+            tokensMiddleware([
+            TIPOS_USUARIOS.CURADOR, 
+            TIPOS_USUARIOS.OPERADOR, 
+            TIPOS_USUARIOS.IDENTIFICADOR,
+            ]),
             listagensMiddleware,
             controller.obtemDadosDoRelatorioDeInventarioDeEspecies,
         ]);
 
     app.route('/relatorio/coleta-por-local-intervalo-de-data')
         .get([
+            tokensMiddleware([
+            TIPOS_USUARIOS.CURADOR, 
+            TIPOS_USUARIOS.OPERADOR, 
+            TIPOS_USUARIOS.IDENTIFICADOR,
+            ]),
             listagensMiddleware,
             controller.obtemDadosDoRelatorioDeColetaPorLocalEIntervaloDeData,
         ]);
