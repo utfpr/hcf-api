dbaf323c4dbf9958bd2054e53d873f05dd968d1b
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2025-08-21 14:59:27 -0300

    fix: lint warnings

--- src/database/migration/20250816024121_add_exsicata_tipo.ts
+++ src/database/migration/20250816024121_add_exsicata_tipo.ts
@@ -1,28 +1,27 @@
-import { Knex } from "knex";
-import { parseFile } from "fast-csv";
+import { parseFile } from 'fast-csv'
+import { Knex } from 'knex'
 
 export async function run(knex: Knex): Promise<void> {
-  await knex.transaction(async (trx) => {
+  await knex.transaction(async trx => {
     await trx.raw(`
       ALTER TABLE tombos 
       ADD COLUMN exsicata_tipo ENUM('UNICATA', 'DUPLICATA') NULL;
-    `);
+    `)
 
     const rows = parseFile(
-      "src/database/20250816024121_add_exsicata_tipo/tombo_exsicata_tipo.csv",
+      'src/database/20250816024121_add_exsicata_tipo/tombo_exsicata_tipo.csv',
       { headers: true }
-    ).on("error", (error) => console.error(error));
+    )
 
+    /* eslint-disable no-restricted-syntax */
     for await (const row of rows) {
-      const { id, tombo_tipo } = row;
+      const { id, tombo_tipo: tomboTipo } = row
 
-      if (!tombo_tipo) continue;
+      if (!tomboTipo) return
 
-      console.log(`Updating tombo ${id} with exsicata_tipo ${tombo_tipo}`);
-
-      await trx("tombos")
+      await trx('tombos')
         .where({ hcf: id })
-        .update({ exsicata_tipo: tombo_tipo });
+        .update({ exsicata_tipo: tomboTipo })
     }
-  });
+  })
 }
