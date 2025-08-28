31c0969aa03f625a5755bdea2de64a7509b82849
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2025-08-28 13:08:29 -0300

    adiciona cidade_id aos locais de coleta criados por repetição

--- src/database/migration/20250827122308_create_locais_coleta.ts
+++ src/database/migration/20250827122308_create_locais_coleta.ts
@@ -8,7 +8,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'Parque Estadual Serra da Baitaca-Caminho do Itupava. Trilha entre a entrada p/ o antes da entrada para o Morro Pão de Loth'
+        'Parque Estadual Serra da Baitaca-Caminho do Itupava. Trilha entre a entrada p/ o antes da entrada para o Morro Pão de Loth',
+      cidade_id: 4120804
     })
 
     await trx('tombos')
@@ -21,7 +22,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'Parque Estadual Serra da Baitaca-Caminho do Itupava. Trilha entre a entrada p/ o Morro Pão de Loth até as Ruínas do Ipiranga'
+        'Parque Estadual Serra da Baitaca-Caminho do Itupava. Trilha entre a entrada p/ o Morro Pão de Loth até as Ruínas do Ipiranga',
+      cidade_id: 4120804
     })
 
     await trx('tombos')
@@ -34,7 +36,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; SMPW. Q.26, conj. 3. Próximo à A.E.E. (EMBRAPA)'
+        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; SMPW. Q.26, conj. 3. Próximo à A.E.E. (EMBRAPA)',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -47,7 +50,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; Lagoa do Córrego do Cedro. Em frente ao SMPW Q.16 conj. 1'
+        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; Lagoa do Córrego do Cedro. Em frente ao SMPW Q.16 conj. 1',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -60,7 +64,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; Park Way. Clube da EMBRAPA. Q. 26. Conjunto 3 (área pública)'
+        'APA Gama-Cabeça de Veado.ARIE do Córrego do Cedro. R.A. do Núcleo Bandeirante; Park Way. Clube da EMBRAPA. Q. 26. Conjunto 3 (área pública)',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -73,7 +78,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Q 26 entre o conjunto 10 e a EMBRAPA (A.E.E)'
+        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Q 26 entre o conjunto 10 e a EMBRAPA (A.E.E)',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -94,7 +100,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Lagoa do Córrego do Cedro. Q 16. Conj. 01. Lote 4'
+        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Lagoa do Córrego do Cedro. Q 16. Conj. 01. Lote 4',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -115,7 +122,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Park Way. Clube da EMBRAPA. Q 26. Conjunto 3 (área pública)'
+        'APA Gama-Cabeça de Veado. ARIE Córrego do Cedro-R.A. Núcleo Bandeirante. SMPW Park Way. Clube da EMBRAPA. Q 26. Conjunto 3 (área pública)',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -136,7 +144,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama e Cabeça de Veado, R.A. do Lago Sul. Fazenda Água Limpa (FAL-UnB). Área da "MOA"'
+        'APA Gama e Cabeça de Veado, R.A. do Lago Sul. Fazenda Água Limpa (FAL-UnB). Área da "MOA"',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
@@ -149,7 +158,8 @@ export async function run(knex: Knex): Promise<void> {
 
     res = await trx('locais_coleta').insert({
       descricao:
-        'APA Gama e Cabeça de Veado, R.A. do Lago Sul. Fazenda Água Limpa (FAL-UnB). Área Borda de mata de galeria do Córrego do Macaco, afluente Gama'
+        'APA Gama e Cabeça de Veado, R.A. do Lago Sul. Fazenda Água Limpa (FAL-UnB). Área Borda de mata de galeria do Córrego do Macaco, afluente Gama',
+      cidade_id: 5300108
     })
 
     await trx('tombos')
