4cd135d501a405254ac47d0133d926ad1b960cfb
Matheus Coitinho <mhcl0303@gmail.com>
2025-09-05 16:12:01 -0300

    feat: adiciona middleware de listagens ao módulo de locais

--- src/routes/locais.js
+++ src/routes/locais.js
@@ -3,6 +3,7 @@ import validacoesMiddleware from '../middlewares/validacoes-middleware';
 import localColetaCadastroEsquema from '../validators/localColeta-cadastro';
 import localColetaListagemEsquema from '../validators/localColeta-listagem';
 import nomeEsquema from '../validators/nome-obrigatorio';
+import listagensMiddleware from '~/middlewares/listagens-middleware';
 
 const controller = require('../controllers/locais-coleta-controller');
 
