09feed8db9be723d00bbc55aa98a82e70d1b7624
Matheus Coitinho <mhcl0303@gmail.com>
2025-10-08 18:53:07 -0300

    Remoção da coluna 'ativo' das colunas

---
+++ src/database/migration/20251008212623_rem_campo_ativo.ts
@@ -0,0 +1,30 @@
+import { Knex } from 'knex'
+
+export async function run(knex: Knex): Promise<void> {
+  await knex.transaction(async trx => {
+    const tabelas = [
+      'autores',
+      'coletores', 
+      'especies',
+      'familias',
+      'generos',
+      'herbarios',
+      'sub_especies',
+      'sub_familias',
+      'telefones',
+      'tombos_fotos',
+      'usuarios',
+      'variedades'
+    ]
+
+    for (const tabela of tabelas) {
+      const hasColumn = await trx.schema.hasColumn(tabela, 'ativo')
+      
+      if (hasColumn) {
+        await trx.schema.alterTable(tabela, table => {
+          table.dropColumn('ativo')
+        })
+      }
+    }
+  })
+}
