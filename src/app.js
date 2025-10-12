a3d562eddd505061bbcafb5eda1e14eaddbfccaf
Edvaldo Szymonek <edvaldoszy@gmail.com>
2025-10-12 17:27:08 -0300

    aumenta rate limit

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.use(cors(corsOptions));
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 500, // Limit each IP to 100 requests per windowMs
     message: {
         error: {
             code: 429,
