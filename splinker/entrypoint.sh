94e88178d11d82384e8d34960d19dd80c2a52b22
Edvaldo Szymonek <edvaldoszy@gmail.com>
2026-04-21 23:22:04 -0300

    melhora dockerfile e variáveis

--- splinker/entrypoint.sh
+++ splinker/entrypoint.sh
@@ -4,16 +4,16 @@ echo "Generating config from environment variables..."
 cat > /app/splinker.conf <<EOF
 [dataset]
 token=${SPLINKER_TOKEN}
-host=${SPLINKER_DATABASE_HOST}
-port=${SPLINKER_DATABASE_PORT}
-dbname=${SPLINKER_DATABASE_NAME}
-user=${SPLINKER_DATABASE_USERNAME}
-password=${SPLINKER_DATABASE_PASSWORD}
+host=${DATABASE_HOST}
+port=${DATABASE_PORT}
+dbname=${DATABASE_NAME}
+user=${DATABASE_USERNAME}
+password=${DATABASE_PASSWORD}
 EOF
 
 echo "Configuring cron job with schedule: $CRON_SCHEDULE ($TZ)"
 
-echo "$CRON_SCHEDULE java -jar /app/splinker.jar /app/splinker.conf 1> /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
+echo "$CRON_SCHEDULE $(which java) -jar /app/splinker.jar /app/splinker.conf 1> /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 if ! crontab -l >/dev/null 2>&1; then
   echo "Failed to configure cron job"
