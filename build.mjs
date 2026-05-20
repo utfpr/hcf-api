e1bad675ed9bac97fe8c6037cfdfba9b68473939
Josue Modesto <josuemodesto129@gmail.com>
2026-05-20 13:09:51 -0300

    fix: remove a versao do yarn que era o problema

--- build.mjs
+++ build.mjs
@@ -1,12 +1,12 @@
 import { rmSync } from 'node:fs'
 import { globSync } from 'glob'
 import { build } from 'esbuild'
-
+import fs from 'node:fs'
+import path from 'node:path'
 
 rmSync('dist', { recursive: true, force: true }) // Limpa a pasta dist antes de gerar novo build
 
-
-const entryPoints = globSync('src/**/*.{js,ts,ejs,tsx}') // Encontra todos os arquivos a serem transpilados
+const entryPoints = globSync('src/**/*.{js,ts,tsx}') // Encontra todos os arquivos a serem transpilados
 
 await build({
     entryPoints,
