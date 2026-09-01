418f3ac5b0c875618ae0b3cbeefc097690116a88
josuemc <josuemodesto@alunos.utfpr.edu.br>
2026-09-01 13:13:39 -0300

    fix: Correção do erro de dump de prod

--- src/database/migration/20260826120203_rename_fase_sucessional_numero_to_id.ts
+++ src/database/migration/20260826120203_rename_fase_sucessional_numero_to_id.ts
@@ -1,9 +1,14 @@
 import { Knex } from 'knex'
 
 export async function run(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('fase_sucessional', table => {
-    table.renameColumn('numero', 'id')
-  })
+  const hasNumero = await knex.schema.hasColumn('fase_sucessional', 'numero')
+  const hasId = await knex.schema.hasColumn('fase_sucessional', 'id')
+
+  if (hasNumero && !hasId) {
+    await knex.schema.alterTable('fase_sucessional', table => {
+      table.renameColumn('numero', 'id')
+    })
+  }
 
   await knex.raw(`
     CREATE SEQUENCE IF NOT EXISTS fase_sucessional_id_seq;
@@ -14,18 +19,16 @@ export async function run(knex: Knex): Promise<void> {
       'fase_sucessional_id_seq',
       COALESCE((SELECT MAX(id) FROM fase_sucessional), 1),
       true
-    )
+    );
   `)
 
-  await knex.schema.alterTable('fase_sucessional', table => {
-    table.bigInteger('id')
-      .notNullable()
-      .defaultTo(knex.raw('nextval(\'fase_sucessional_id_seq\')'))
-      .alter()
-  })
+  await knex.raw(`
+    ALTER TABLE fase_sucessional
+    ALTER COLUMN id SET DEFAULT nextval('fase_sucessional_id_seq');
+  `)
 
   await knex.raw(`
     ALTER SEQUENCE fase_sucessional_id_seq
-    OWNED BY fase_sucessional.id
+    OWNED BY fase_sucessional.id;
   `)
 }
